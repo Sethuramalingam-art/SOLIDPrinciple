d91864080120d3b21f1a57bf790ba3b5e1d95287
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-06-16 23:13:01 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import SingleResponsibiltyPrinciple from "./SOLID/SingleResponsibiltyPrinciple";
+
+function App() {
+  return (
+    <div className="App">
+      <SingleResponsibiltyPrinciple />
+    </div>
+  );
+}
+
+export default App;
---
+++ SingleResponsibiltyPrinciple.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SingleResponsibiltyPrinciple() {
+  return <div>SingleResponsibiltyPrinciple</div>;
+}
