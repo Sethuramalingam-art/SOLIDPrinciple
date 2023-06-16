31c7fa1a16b7c652dceb9e5a034933a2f058a05b
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-06-16 23:59:05 +0530

    Update SingleResponsibiltyPrinciple.tsx

--- SingleResponsibiltyPrinciple.tsx
+++ SingleResponsibiltyPrinciple.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect, useState, useReducer } from "react";
 
-export default function SingleResponsibiltyPrinciple() {
-  return <div>SingleResponsibiltyPrinciple</div>;
+interface initialSate {
+  isLoading: Boolean;
 }
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+const intialState: initialSate = {
+  isLoading: true,
+};
+
+//Comples State Managment
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case "LOADING":
+      return { isLoading: true };
+    case "FINISHED":
+      return { isLoading: false };
+    default:
+      return state;
+  }
+}
+
+const SingleResponsibiltyPrinciple = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+  return <div>SingleResponsibiltyPrinciple</div>;
+};
+
+export default SingleResponsibiltyPrinciple;
